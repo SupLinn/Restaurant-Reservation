1fa269515140bbb99611101a8dd37a15377cd48e
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2024-12-13 23:16:06 +0530

    add Pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import Home from './Pages/Home'
+import Notfound from './Pages/Notfound'
+import Success from './Pages/Success'
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element = {}/>
-        <Route path="/" element = {}/>
-        <Route path="/" element = {}/>
+        <Route path="/" element = {Home}/>
+        <Route path="*" element = {Notfound}/>
+        <Route path="/success" element = {Success}/>
       </Routes>
       <Toaster/>
     </Router>
